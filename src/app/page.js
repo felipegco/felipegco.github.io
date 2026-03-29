55f145739fe75c773341c754fdb3f8924799bf73
Felipe Giacomini Cocco <felipegco@proton.me>
2026-03-29 17:38:41 -0300

    Update: Add language-based redirection to `/pt` and `/en` routes

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
 import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
 
 export default function RootPage() {
+    const headersList = headers();
+
+    const acceptLanguage = headersList.get('accept-language') || '';
+
+    if (acceptLanguage.toLowerCase().includes('pt')) {
+        redirect('/pt');
+    }
     redirect('/en');
 }
